b0b3eb8055a9dd519642ebb213f0b670634515b1
lab <manoel.dahan@gmail.com>
2017-10-23 17:10:33 +0200

    display of data on front end

--- routes/user.js
+++ routes/user.js
@@ -6,18 +6,30 @@ var mongoose = require('mongoose');
 router.get('/:id', function(req, res, next) {
 
   //TODO: récupérer en base les données de l'ID
-    mongoose.model('contacts').findOne({'_id': req.id },'firstName lastName company', function(err, contact){
+    console.log(req.params.id);
+    mongoose.model('contacts').findOne({'_id': req.params.id }, function(err, contact){
         if(err) throw err;
         if (contact != null) {
 
             console.log(contact);
 
             console.log('%s %s %s', contact.firstName, contact.lastName, contact.company);
+
+            res.render('users', {
+                Id: req.params.id ,
+                fname: contact.firstName ,
+                lname: contact.lastName,
+                bday: contact.birthDate,
+                company: contact.company,
+                emails: contact.emails
+            } );
         }
+        else
+            res.redirect('/')
     });
 
 
-  res.render('users', { "Id": req.id } );
+
 });
 
 module.exports = router;
