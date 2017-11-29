48077a42c3030e0f9902064e136c47f5b746329c
lab <manoel.dahan@gmail.com>
2017-11-29 05:15:22 +0100

    sort & limit

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,59 @@ var express = require('express');
 var router = express.Router();
 var mongo = require('../model/contacts')
 
+
+const   DefaultSort     = null,
+        DefaultLimit    = 20;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log(req.query.li );
+
+    switch(req.query.s) {
+        case 'cmpy':
+            var sort = {'company': 1};
+            break;
+        case 'lname':
+            var sort = {'lastName': 1};
+            break;
+        default:
+            var sort = DefaultSort;
+    }
+
+    switch(req.query.li) {
+        case '40':
+            var limit = 40;
+            break;
+        case '60':
+            var limit = 60;
+            break;
+        case 'all':
+            var limit = null;
+            break;
+        default:
+            var limit = DefaultLimit;
+    }
+
+    console.log(limit)
 
   //TODO: send data about contact list
     mongo.find({},'_id firstName lastName company', function(err, contacts){
-      if(err) throw err;
-      //res.send(contacts);
+        if(err) throw err;
+        //res.send(contacts);
 
-     var tab = new Array();
-     var ids = new Array();
+        var tab = new Array();
+        var ids = new Array();
 
-     contacts.forEach(function(contact,i){
-         var a = { "fname":contact.firstName , "lname":contact.lastName , "company":contact.company };
-         tab[i] = a;
-         ids[i] = contact._id
-          //console.log('%s %s %s', contact.firstName, contact.lastName, contact.company);
-      })
+        contacts.forEach(function(contact,i){
+            var a = { "fname":contact.firstName , "lname":contact.lastName , "company":contact.company };
+            tab[i] = a;
+            ids[i] = contact._id
+            //console.log('%s %s %s', contact.firstName, contact.lastName, contact.company);
+        })
 
-     res.render('index', { contacts: tab , identities: ids});
+        res.render('index', { contacts: tab , identities: ids});
 
- });
+    }).sort(sort).limit(limit);
 
 
 });
